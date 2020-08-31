99097bda08a91c3123a7c43d92e5a9581d01c316
Ridhanya <39475050+Ridhanya@users.noreply.github.com>
2020-08-31 22:05:57 +0530

    posting purposes with instabot - working

---
+++ insta_post.py
@@ -0,0 +1,10 @@
+
+
+from instabot import Bot 
+bot = Bot() 
+
+bot.login(username = "____gene____", 
+		password = "gene@8654")
+bot.upload_photo("D:/Instagram-bot-master/space.jpeg", 
+		caption = "provide the caption that you want to display on the post here") 
+
