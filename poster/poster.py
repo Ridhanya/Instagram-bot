8d7df7fbea806c015a11e9a3c7920beed15382b8
Ridhanya <39475050+Ridhanya@users.noreply.github.com>
2020-09-02 09:29:35 +0530

    text wrap feature with pillow

--- poster/poster.py
+++ poster/poster.py
@@ -1,106 +1,104 @@
+from PIL import Image, ImageDraw, ImageFont
+from random import randint
+import pandas
+import random
+#create image object from the input image path
+def text_wrap(text, font, max_width):
+        """Wrap text base on specified width. 
+        This is to enable text of width more than the image width to be display
+        nicely.
+        @params:
+            text: str
+                text to wrap
+            font: obj
+                font of the text
+            max_width: int
+                width to split the text with
+        @return
+            lines: list[str]
+                list of sub-strings
+        """
+        lines = []
+        
+        # If the text width is smaller than the image width, then no need to split
+        # just add it to the line list and return
+        if font.getsize(text)[0]  <= max_width:
+            lines.append(text)
+        else:
+            #split the line by spaces to get words
+            words = text.split(' ')
+            i = 0
+            # append every word to a line while its width is shorter than the image width
+            while i < len(words):
+                line = ''
+                while i < len(words) and font.getsize(line + words[i])[0] <= max_width:
+                    line = line + words[i]+ " "
+                    i += 1
+                if not line:
+                    line = words[i]
+                    i += 1
+                lines.append(line)
+        return lines
 
 
-# load desired font for the poster
-import matplotlib.font_manager as fm
-def findFont(name='Arial'):
-    possiblefonts = fm.findSystemFonts()
-    return [f for f in possiblefonts if name in f]
-prop = fm.FontProperties(fname='D:\\arial\\arial.ttf')
-
-# set the font to that font
-import matplotlib
-matplotlib.rcParams['font.family'] = prop.get_name()
-
-# import various tools
-import glob, os
-import matplotlib.pyplot as plt, scipy.misc, numpy as np
-import imageio
 
 
-def makePosters(strings = open('phrases.txt').readlines(),
-                images = glob.glob('/Users/zkbt/Cosmos/Artwork/AstroImages/*')):
-    '''Make a bunch of posters from a list of strings, choosing a random image for each.'''
+
+try:
+	n=random.randint(1,7)
+	image = Image.open("D:\\INSTA\\space_images\\space"+str(n)+".jfif")   
+except IOError as e:
+    print(e)
+
+# Resize the image 
+df=pandas.read_csv("D:\\INSTA\\scrap\\two.csv")
+li=[]
+for row in df["Headline"]:
+	li.append(row)
+width = 500
+img_w = image.size[0]
+img_h = image.size[1]
+wpercent = (width/float(img_w))
+hsize = int((float(img_h)*float(wpercent)))
+rmg = image.resize((width,hsize), Image.ANTIALIAS)
+
+# Set x boundry
+# Take 10% to the left for min and 50% to the left for max
+x_min = (rmg.size[0] * 8) // 100
+x_max = (rmg.size[0] * 50) // 100
+# Randomly select x-axis
+ran_x = randint(x_min, x_max)
+# Create font object with the font file and specify desired size
+# Font style is `arial` and font size is 20
+font_path = 'font/arialbd.ttf'
+font = ImageFont.truetype(font=font_path, size=40)
+text = str(li[0])
+lines = text_wrap(text, font, rmg.size[0]-ran_x)
+line_height = font.getsize('hg')[1]
+
+y_min = (rmg.size[1] * 4) // 100   # 4% from the top
+y_max = (rmg.size[1] * 90) //100   # 90% to the bottom
+y_max -= (len(lines)*line_height)  # Adjust
+ran_y = randint(y_min, y_max)      # Generate random point
+
+#Create draw object
+draw = ImageDraw.Draw(rmg)
+#Draw text on image
+color = 'rgb(110,150,240)'  # Red color
+x = ran_x
+y = ran_y
+for line in lines:
+    draw.text((x,y), line, fill=color, font=font)
     
-    poster(strings, filename=np.random.choice(images))
+    y = y + line_height    # update y-axis for new line
+# Redefine x and y-axis to insert author's name
+
+y += 5                       # Add some line space
+x += 20   
+
+rmg=rmg.resize((1080,1080))                   # Indent it a bit to the right
+
+rmg.save("output.jpg")
 
 
-def poster(string='Hubble telescope catches stunning view of Comet NEOWISE after its spectacular summer sky show', filename='D:\\Instagram-bot-master\\space.jpeg',
-            size=(8.5, 11), margin=0.5, dpi=100, channel='green'):
-    '''Make a poster, given a string and an image.
-    string = what to say
-    filename = image to show
-    size = (width, height) of poster
-    margin = white space from edge of poster (same all around)
-    dpi = resolution (probbaly best to keep around 100)
-    channel = which of RGB colors to use, in case the input image is in color'''
 
-    plt.ion()
-    print
-    print ("Making a poster that says:")
-
-    # convert double spaces into newlines
-    string = string.replace('  ', '\n')
-    print ("--------------------------")
-    print (string)
-    print ("--------------------------")
-
-    # define some scales
-    xsize = float(size[0])
-    ysize = float(size[1])
-    aspect = ysize/xsize
-
-    # load the image
-    image = imageio.imread(filename)
-    print ("Using {0}, a {1} image, as a backdrop.".format(filename, image.shape))
-
-    # create the plot
-    plt.figure('poster', figsize=size, dpi=dpi)
-    a = plt.subplot()
-    a.cla()
-    plt.setp(a.get_xticklabels(), visible=False)
-    plt.setp(a.get_xticklines(), visible=False)
-    plt.setp(a.get_yticklabels(), visible=False)
-    plt.setp(a.get_yticklines(), visible=False)
-    plt.subplots_adjust(left=margin/xsize, right=1.0 - margin/xsize, top=1.0 - margin/ysize, bottom=margin/ysize)
-
-    # show the image, in grayscale, cropping to center
-    if len(image.shape) == 2:
-        print ("The image appears to be black and white. How easy!")
-    elif len(image.shape) == 3:
-        rgb = dict(red=0, green=1, blue=2)
-        print ("The image appears to be a color image. Using channel={0} as black-and-white.".format(channel))
-        image = image[:,:,rgb[channel.lower()]]
-    else:
-        print ("The image has a confusing shape [{0}]. Skipping it!".format(image.shape))
-        return
-
-    # crop the image to the right aspect ratio
-    imageaspect = float(image.shape[0])/float(image.shape[1])
-    if imageaspect > aspect:
-        # (image is taller than the poster)
-        trim = int((image.shape[0] - aspect*image.shape[1])/2)
-        image = image[trim:-trim, :]
-    elif imageaspect < aspect:
-        # (image is wider than the poster)
-        trim = int((image.shape[1] - image.shape[0]/aspect)/2)
-        image = image[:,trim:-trim]
-    print (imageaspect)
-
-    # draw the background image, using vmax=500 to keep all colors dark gray
-    a.imshow(image, cmap='gray', interpolation='nearest', extent=[0, xsize, 0, ysize], vmax=500)
-
-    # draw the text
-    a.text(4.25, 0.75, 'copyrights @ spacebot', fontsize=30, ha='center', weight='normal', color='white')
-    
-    a.text(4.25, 9, string.upper(), fontsize=30, color='white', ha='center', weight='normal', va='top')
-
-    # save the output posters to their own directory
-    
-    #os.mkdir('outputs')
-    
-    filename ='post' + '.jpg'
-    print ("Saving poster to ", filename)
-    plt.savefig(filename)
-    plt.draw()
-
-poster()
